658f19072b7bcce3e72a1ccf98fbef676148d074
Nevcan Uludas <nevcan@uludas.com>
2019-07-01 17:25:50 +0300

    Fix, overflow rotating

--- tetris.js
+++ tetris.js
@@ -42,7 +42,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = 'red';
+        context.fillStyle = 'green';
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
@@ -76,6 +76,42 @@ function playerMove (dir) {
   }
 }
 
+function playerRotate (dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while (collide (arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+function rotate (matrix, dir) {
+  for (let y = 0; y < matrix.length; ++y) {
+    for (let x = 0; x < y; ++x) {
+      [
+        matrix[x][y],
+        matrix[y][x],
+      ] = [
+        matrix[y][x],
+        matrix[x][y],
+      ];
+    }
+  }
+
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -86,8 +122,7 @@ function update (time = 0) {
 
   dropCounter += deltaTime;
   if (dropCounter > dropInterval) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop ();
   }
 
   draw ();
@@ -98,7 +133,7 @@ const arena = createMatrix(12, 20);
 
 const player = {
   pos: {x: 4, y: 0},
-  matrix: matrix,
+  matrix: matrix
 }
 
 document.addEventListener ('keydown', event => {
@@ -108,6 +143,10 @@ document.addEventListener ('keydown', event => {
     playerMove (1);
   } else if (event.keyCode === 40) {
     playerDrop ();
+  } else if (event.keyCode === 81) {
+    playerRotate(-1);
+  } else if (event.keyCode === 87) {
+    playerRotate(1);
   }
 }) 
 
