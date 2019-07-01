6d508928de4916eae04eba085ab06f672142da12
Nevcan Uludas <nevcan@uludas.com>
2019-07-01 13:59:18 +0300

    Fix, matrix value

--- tetris.js
+++ tetris.js
@@ -10,6 +10,26 @@ const matrix = [
   [0, 1, 0],
 ];
 
+function collide (arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; x++) {
+      if (m[y][x] !== 0 && ( arena[y + o.y] && arena[y +o.y][x + o.x] ) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix (w, h) {
+  const matrix = [];
+  while (h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
 function draw() {
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -26,6 +46,26 @@ function drawMatrix(matrix, offset) {
       }
     });
   });
+}
+
+function merge (arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
+function playerDrop () {
+  player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
 }
 
 let dropCounter = 0;
@@ -46,6 +86,8 @@ function update (time = 0) {
   requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+
 const player = {
   pos: {x: 4, y: 0},
   matrix: matrix,
@@ -57,8 +99,7 @@ document.addEventListener ('keydown', event => {
   } else if (event.keyCode === 39) {
     player.pos.x++;
   } else if (event.keyCode === 40) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop ();
   }
 }) 
 
