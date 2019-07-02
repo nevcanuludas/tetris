2aad6d9ef4e34cf3d331eea802a43eefa9b9ed71
Nevcan Uludas <nevcan@uludas.com>
2019-07-02 16:27:37 +0300

    Fix, score calc

--- tetris.js
+++ tetris.js
@@ -4,6 +4,7 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 function arenaSweep () {
+  let rowCount = 1;
   outer: for (let y = arena.length - 1; y > 0; --y) {
     for (let x = 0; x < arena[y].length; ++x) {
       if (arena[y][x] === 0) {
@@ -14,6 +15,9 @@ function arenaSweep () {
     const row = arena.splice(y, 1)[0].fill(0);
     arena.unshift(row);
     ++y;
+
+    player.score += rowCount * 10;
+    rowCount *= 2;
   }
 }
 
@@ -120,6 +124,7 @@ function playerDrop () {
     merge (arena, player);
     playerReset ();
     arenaSweep ();
+    updateScore ();
   }
   dropCounter = 0;
 }
@@ -138,6 +143,8 @@ function playerReset () {
   player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
   if (collide(arena, player)) {
     arena.forEach(row => row.fill(0));
+    player.score = 0;
+    updateScore ();
   }
 }
 
@@ -194,6 +201,10 @@ function update (time = 0) {
   requestAnimationFrame(update);
 }
 
+function updateScore () {
+  document.getElementById('score').innerHTML = player.score;
+}
+
 const colors = [
   null,
   '#FF0D72',
@@ -208,8 +219,9 @@ const colors = [
 const arena = createMatrix(12, 20);
 
 const player = {
-  pos: {x: 4, y: 0},
-  matrix: createPiece('T'),
+  pos: {x: 0, y: 0},
+  matrix: null,
+  score: 0, 
 }
 
 document.addEventListener ('keydown', event => {
@@ -226,4 +238,6 @@ document.addEventListener ('keydown', event => {
   }
 }) 
 
+playerReset ();
+updateScore ();
 update ();
